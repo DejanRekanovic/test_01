c4db10faa5f8ef439d26b2de4339bcbfb7ab20e6
DejanRex <dejanrex992@gmail.com>
2021-07-06 17:32:27 +0200

    new file:   hello.c 	new file:   hello.exe 	renamed:    hello_world -> hello_world.c 	new file:   hello_world.exe

---
+++ hello.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("djes\n");
+    return 0;
+}
--- hello_world.c
+++ hello_world.c
@@ -3,5 +3,6 @@
 int main()
 {
   printf("Hello World\n");
+  printf("Moze li i ovako ? \n");
   return 0;
 }
